cc9c617bd3d656b1c49cde5e57ea87fbac5c3d94
BagboBillbins <33336191+BagboBillbins@users.noreply.github.com>
2018-09-26 15:19:52 -0500

    Added Movement for Player Character

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -17,7 +17,8 @@ public class PlayerCharacter : MonoBehaviour {
 
     [SerializeField]
     private bool hasKey, isOnGround;
-    private float thrust;
+    //private float thrust;
+    private float horzInput;
 
     private Rigidbody2D rigbody2D;
     
@@ -26,6 +27,7 @@ public class PlayerCharacter : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+        //have to initialize rigidbody or will throw null exception
         rigbody2D = GetComponent<Rigidbody2D>();
 	}
 	
@@ -33,7 +35,19 @@ public class PlayerCharacter : MonoBehaviour {
 	void Update ()
     {
         rigbody2D.gravityScale = 5;
+
+        GetInput();
+        Move();
+        
         //transform.Translate(0, -0.1f, 0); // does not use physics
         //rigbody2D.AddForce(transform.forward * thrust);// uses physics
 	}
+    private void GetInput()
+    {
+        horzInput = Input.GetAxis("Horizontal");
+    }
+    private void Move()
+    {
+        rigbody2D.velocity = new Vector2(horzInput, 0);
+    }
 }
