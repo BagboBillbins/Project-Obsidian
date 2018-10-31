7c5d22e2da03e559cb38871cb268e62bc1d2e207
BagboBillbins <33336191+BagboBillbins@users.noreply.github.com>
2018-10-31 15:25:07 -0500

    Added default spirte, added idle and move animation, updated for smoother movement in scripts

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -8,46 +8,102 @@ public class PlayerCharacter : MonoBehaviour {
 
     [SerializeField]
     private int lives = 3;
-
     [SerializeField]
     private string name = "";
-
     [SerializeField]
-    private float jumpHeight = 5, speed = 5;
-
+    private float jumpForce = 5, speed = 5;
     [SerializeField]
-    private bool hasKey, isOnGround;
-    //private float thrust;
+    private ContactFilter2D groundContactFilter;
+    [SerializeField]
+    private Collider2D groundDetectTrig;
+    [SerializeField]
+    private Collider2D playerGroundCollider;
+    [SerializeField]
+    private PhysicsMaterial2D playerMovePhys, playerStopPhys;
+
     private float horzInput;
+    private float jump;
+    public float maxSpeed = 10f;
+    private bool facingRight;
+    private bool onGround;
 
-    private Rigidbody2D rigbody2D;
+    private Collider2D[] groundHitDetector = new Collider2D[16];
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
     
 
-    
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         //have to initialize rigidbody or will throw null exception
-        rigbody2D = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 	}
-	
 	// Update is called once per frame
 	void Update ()
     {
-        rigbody2D.gravityScale = 5;
+        rb2d.gravityScale = 5;
+        UpdateOnGround();
+        UpdateHorzInput();
+        JumpInputHandler();
 
-        GetInput();
-        Move();
-        
+
         //transform.Translate(0, -0.1f, 0); // does not use physics
         //rigbody2D.AddForce(transform.forward * thrust);// uses physics
-	}
-    private void GetInput()
+    }
+    void FixedUpdate()
     {
-        horzInput = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(horzInput));
+
+        UpdatePhysMat();
+        Move();
+        if (horzInput < 0 && !facingRight)
+            Flip();
+        else if (horzInput > 0 && facingRight)
+            Flip();
+        
+    }
+
+
+    private void UpdateHorzInput()
+    {
+        horzInput = Input.GetAxisRaw("Horizontal"); //raw ignores unity's smoothing filter which makes movement more responsive
+    }
+
+    private void JumpInputHandler()
+    {
+        if (Input.GetButtonDown("Jump") && onGround)
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);//Impulse adds immediate action while force is not
+
     }
     private void Move()
     {
-        rigbody2D.velocity = new Vector2(horzInput, 0);
+        rb2d.AddForce(Vector2.right * horzInput * speed);
+        Vector2 clampedVelocity = rb2d.velocity;//set clamp for velocity
+        clampedVelocity.x = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);//clamp the max speed on the x axis
+        rb2d.velocity = clampedVelocity;
     }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    private void UpdatePhysMat()
+    {
+        if (Mathf.Abs(horzInput) > 0)
+            playerGroundCollider.sharedMaterial = playerMovePhys;
+
+        else
+            playerGroundCollider.sharedMaterial = playerStopPhys;
+    }
+    private void UpdateOnGround()
+    {
+        onGround = groundDetectTrig.OverlapCollider(groundContactFilter, groundHitDetector) > 0;
+        //Debug.Log("Grounded: " + onGround);
+
+    }
+
 }
