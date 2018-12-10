32b1bc60de58488d2d57043617c47315bb9fac58
BagboBillbins <33336191+BagboBillbins@users.noreply.github.com>
2018-12-09 22:10:19 -0600

    Stylized Main Menu

--- Assets/Scripts/SelectOnInput.cs
+++ Assets/Scripts/SelectOnInput.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SelectOnInput : MonoBehaviour
 {
     public EventSystem eventSystem;
     public GameObject selectedObject;
